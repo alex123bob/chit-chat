df5f3338f7e3022b9cc66e3bb0311beea9601be3
Alexander Li <alexander.li@ef.com>
2016-10-27 09:15:26 +0800

    make a trial for nodejs and comment some logger parameter for backup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.use('/category/:ctId', function (req, res, next){
+  req.output = 'the request category Id is: ' + req.params.ctId + '\n';
+  next();
+});
+
+router.get('/category/:ctId', function (req, res, next){
+  if (req.params.ctId == 1) {
+    next('route');
+  }
+  else {
+    next();
+  }
+}, function (req, res, next){
+  res.send(req.output.replace(/\n/gi, '<br />'));
+});
+
+router.get('/category/:ctId', function (req, res, next){
+  res.send('you are our first user whose id is 1');
 });
 
 module.exports = router;
