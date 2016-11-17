be61e747f3016d086571520cfc97ac80d907a2b2
alex123bob <alex123bob@126.com>
2016-11-17 17:35:44 +0800

    add user deletion function(soft deletion) after quiting current game

--- gary/config.js
+++ gary/config.js
@@ -13,16 +13,11 @@ exports.setupSocket = (server) => {
     });
     
     io.on('connection', (socket) => {
-        socket.on('userMessage', (data) => {
-            socket.emit('userMessage', data);
-            socket.broadcast.emit('userMessage', data);
-        });
-
         socket.on('userLogin', (data) => {
             players[data.username] = data;
             status[data.username] = 'normal';
             userInfoBySocketId[socket.id] = {
-                username: data.username, 
+                username: data.username,
                 nickname: data.nickname
             };
             sockets[data.username] = socket;
@@ -31,37 +26,33 @@ exports.setupSocket = (server) => {
         });
 
         socket.on('userUpdate', (data) => {
-            if (data) {
+            if (data && players[data.username]) {
                 players[data.username].coor = data.coor;
             }
             io.emit('userUpdate', players);
         });
 
-        socket.on('userLogout', (data) => {
-            console.log('user: ' + data.nickname + ' has left.');
-            socket.emit('userLogout', data);
-            socket.broadcast.emit('userLogout', data);
-        });
-
         socket.on('disconnect', (data) => {
             let userInfo = userInfoBySocketId[socket.id];
-            delete players[userInfo.username];
-            delete status[userInfo.username];
-            delete sockets[userInfo.username];
+            if (userInfo && userInfo.username) {
+                status[userInfo.username] = 'deleted';
+                players[userInfo.username]['isdeleted'] = true;
+            }
+            io.emit('userUpdate', players);
         });
 
-        socket.on('sendChallenge', (data) => {
-            let sender = players[data.sender.username],
-                receiver = players[data.receiver.username];
-            if (status[data.receiver.username] == 'challenge') {
-                socket.emit('challengeNotAvailable', receiver);
-            } 
-            else {
-                status[data.sender.username] = 'challenge';
-                status[data.receiver.username] = 'challenge';
-                socket.emit('waitingChallengeAccept', receiver);
-                sockets[data.receiver.username].emit('challengeAccept', sender);
-            }
-        });
+        // socket.on('sendChallenge', (data) => {
+        //     let sender = players[data.sender.username],
+        //         receiver = players[data.receiver.username];
+        //     if (status[data.receiver.username] == 'challenge') {
+        //         socket.emit('challengeNotAvailable', receiver);
+        //     } 
+        //     else {
+        //         status[data.sender.username] = 'challenge';
+        //         status[data.receiver.username] = 'challenge';
+        //         socket.emit('waitingChallengeAccept', receiver);
+        //         sockets[data.receiver.username].emit('challengeAccept', sender);
+        //     }
+        // });
     });
 }
